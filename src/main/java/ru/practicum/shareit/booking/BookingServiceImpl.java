4ed73575e3be36ffd8e153cac971b03b85784485
Ext-K.V.Nazin <TXPplus04->
2022-11-08 13:39:59 +0300

    fix: убрал неиспользуемый импорт

--- src/main/java/ru/practicum/shareit/booking/BookingServiceImpl.java
+++ src/main/java/ru/practicum/shareit/booking/BookingServiceImpl.java
@@ -14,7 +14,6 @@ import ru.practicum.shareit.user.User;
 import ru.practicum.shareit.user.UserRepository;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
